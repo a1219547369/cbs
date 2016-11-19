2b926fb69a616adc88a1f159fe911e10dbc02f09
a1219547369 <1649959673@qq.com>
2016-11-19 16:03:17 +0800

    9

--- src/main/java/AddFollowRiskServlet.java
+++ src/main/java/AddFollowRiskServlet.java
@@ -2,7 +2,6 @@
 
 import java.io.*;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.util.*;
 
 import javax.servlet.*;
@@ -11,8 +10,8 @@ import javax.servlet.http.*;
 
 import com.sun.media.jfxmedia.logging.Logger;
 
-import java.sql.Connection;import java.sql.Connection;
-import java.sql.PreparedStatement;import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 
@@ -24,18 +23,15 @@ public class AddFollowRiskServlet extends HttpServlet {
 		LoginServlet ls=new LoginServlet();
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
 		request.setCharacterEncoding("UTF-8");
 		String riskID = request.getParameter("riskID");
 		String moddes = request.getParameter("moddes");
 		String status = request.getParameter("status");
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");//设置日期格式
 		String creattime=df.format(new Date());// new Date()为获取当前系统时间
-		//String date=starthour+"-"+startmin+"";
 
 		
-		//驱动程序名   
-        String driverName = "com.mysql.jdbc.Driver";  
+  
         //数据库用户名   
         String userName = "root";  
         //密码   
@@ -65,16 +61,12 @@ public class AddFollowRiskServlet extends HttpServlet {
         statement.close();
         statement2.close();
         } catch (SQLException e) {
-			// TODO Auto-generated catch block
 			Logger.logMsg(1,"异常");
 		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
 			Logger.logMsg(1,"异常");
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			Logger.logMsg(1,"异常");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			Logger.logMsg(1,"异常");
 		}  
 		response.sendRedirect("ProjectRiskManagement.jsp");
